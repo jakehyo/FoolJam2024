7e1ca2cee3725570067819de557787c5acdbf0cf
Jeffrey Han <hanjeffr@usc.edu>
2024-03-30 18:53:10 -0700

    Replaced HappyBoo Node with Slime Node for Player Character

--- characters/slime/slime.gd
+++ characters/slime/slime.gd
@@ -4,7 +4,15 @@ extends Node2D
 func play_walk():
 	%AnimationPlayer.play("walk")
 
+func play_idle():
+	%AnimationPlayer.play("idle")
 
 func play_hurt():
 	%AnimationPlayer.play("hurt")
 	%AnimationPlayer.queue("walk")
+
+func is_hurt():
+	print(%AnimationPlayer.assigned_animation)
+	return %AnimationPlayer.assigned_animation == "hurt"
+	
+
