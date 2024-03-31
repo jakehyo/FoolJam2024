53094979eb49857890d06c4da1d690a5ad0a7c78
Jeffrey Han <hanjeffr@usc.edu>
2024-03-31 16:52:03 -0700

    Final push

--- items/item.gd
+++ items/item.gd
@@ -8,9 +8,9 @@ var item
 
 func _ready():
 	#randomize weapon obtained.
-	items = [preload("res://items/sword.tscn"),preload("res://items/gun.tscn"), preload("res://items/gun.tscn"), preload("res://items/gun.tscn"), preload("res://items/gun.tscn")]
-	item_id = randi_range(1,5)
-	item = get_child(item_id)
+	items = [preload("res://items/sword.tscn"),preload("res://items/gun.tscn"), preload("res://items/gun2.tscn"), preload("res://items/gun3.tscn")]
+	item_id = randi_range(0,3)
+	item = get_child(item_id + 1)
 	#set item to visible.
 	item.set_visible(true)
 	#preload more items
@@ -20,6 +20,6 @@ func _on_body_entered(body):
 	queue_free()
 	print("Item Collected")
 	print("Body: " + body.name)
-	var collectable = items[item_id-1]
+	var collectable = items[item_id]
 	body.get_node("Weapons").add_child.call_deferred(collectable.instantiate())
 	
